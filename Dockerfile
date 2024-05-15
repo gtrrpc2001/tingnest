8f6a5dc3f43a83fc0c85c042017f1e0178891b84
gtrrpc2001 <cbdgjs3548@naver.com>
2024-05-15 16:07:22 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -36,7 +36,6 @@
 # ENTRYPOINT npm run start:prod
 
 FROM node:18-alpine
-
 WORKDIR /app
 COPY package*.json ./
 RUN npm install --production
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,24 @@
+const commonEnv = {
+  PORT:process.env.PORT,        
+  HOST:process.env.HOST,        
+  NAME:process.env.NAME,
+  PASSWORD:process.env.PASSWORD,
+  DATABASE:process.env.DATABASE,
+  USER_GUARD_LOGIN:process.env.USER_GUARD_LOGIN,
+  USER_GUARD_LOGOUT:process.env.USER_GUARD_LOGOUT,
+  USER_ACTIVITY_LOGIN:process.env.USER_ACTIVITY_LOGIN,
+  USER_ACTIVITY_LOGOUT:process.env.USER_ACTIVITY_LOGOUT,
+  REDISHOST:process.env.REDISHOST,
+  REDISPORT:process.env.REDISPORT,        
+  REDISDB:process.env.REDISDB,
+  NAVER_ACCESSKEY:process.env.NAVER_ACCESSKEY,
+  NAVER_SECRETKEY:process.env.NAVER_SECRETKEY,
+  NAVER_SERVICEID:process.env.NAVER_SERVICEID,
+  COMPANYNUMBER:process.env.COMPANYNUMBER,
+  JWT_SECRET:process.env.JWT_SECRET,
+  JWT_EXPIRATION_TIME:process.env.JWT_EXPIRATION_TIME
+}
+
 module.exports = {
     apps : [{
       name: "Tingproject",
@@ -9,45 +30,13 @@ module.exports = {
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "development",
-        PORT:process.env.PORT,        
-        HOST:process.env.HOST,        
-        NAME:process.env.NAME,
-        PASSWORD:process.env.PASSWORD,
-        DATABASE:process.env.DATABASE,
-        USER_GUARD_LOGIN:process.env.USER_GUARD_LOGIN,
-        USER_GUARD_LOGOUT:process.env.USER_GUARD_LOGOUT,
-        USER_ACTIVITY_LOGIN:process.env.USER_ACTIVITY_LOGIN,
-        USER_ACTIVITY_LOGOUT:process.env.USER_ACTIVITY_LOGOUT,
-        REDISHOST:process.env.REDISHOST,
-        REDISPORT:process.env.REDISPORT,        
-        REDISDB:process.env.REDISDB,
-        NAVER_ACCESSKEY:process.env.NAVER_ACCESSKEY,
-        NAVER_SECRETKEY:process.env.NAVER_SECRETKEY,
-        NAVER_SERVICEID:process.env.NAVER_SERVICEID,
-        COMPANYNUMBER:process.env.COMPANYNUMBER,
-        JWT_SECRET:process.env.JWT_SECRET,
-        JWT_EXPIRATION_TIME:process.env.JWT_EXPIRATION_TIME
+        BACKENDPORT:process.env.BACKENDPORT,
+        ...commonEnv,
       },
       env_production: {
         NODE_ENV: "production",
-        PORT:process.env.PORT,        
-        HOST:process.env.HOST,        
-        NAME:process.env.NAME,
-        PASSWORD:process.env.PASSWORD,
-        DATABASE:process.env.DATABASE,
-        USER_GUARD_LOGIN:process.env.USER_GUARD_LOGIN,
-        USER_GUARD_LOGOUT:process.env.USER_GUARD_LOGOUT,
-        USER_ACTIVITY_LOGIN:process.env.USER_ACTIVITY_LOGIN,
-        USER_ACTIVITY_LOGOUT:process.env.USER_ACTIVITY_LOGOUT,
-        REDISHOST:process.env.REDISHOST,
-        REDISPORT:process.env.REDISPORT,        
-        REDISDB:process.env.REDISDB,
-        NAVER_ACCESSKEY:process.env.NAVER_ACCESSKEY,
-        NAVER_SECRETKEY:process.env.NAVER_SECRETKEY,
-        NAVER_SERVICEID:process.env.NAVER_SERVICEID,
-        COMPANYNUMBER:process.env.COMPANYNUMBER,
-        JWT_SECRET:process.env.JWT_SECRET,
-        JWT_EXPIRATION_TIME:process.env.JWT_EXPIRATION_TIME
+        BACKENDPORT:process.env.DEFAULT,
+        ...commonEnv,
     }
     }]
   };
